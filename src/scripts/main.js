4fd68c96c122c25c21c9bd81bbc0a32cb193a81a
Andrei Nikiporchyk <andrewfroze@gmail.com>
2026-09-15 20:11:18 +0400

    feat: create initial layout with applying saved theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,49 @@
-console.log("Coffee House app");
+import "../styles/main.scss";
+import { darkTheme, lightTheme } from "./themes";
+
+let activeTheme;
+
+function loadActiveTheme() {
+  activeTheme = localStorage.getItem("theme");
+
+  if (!activeTheme || !["light", "dark"].includes(activeTheme)) {
+    activeTheme = "light";
+    localStorage.setItem("theme", activeTheme);
+  }
+
+  switch (activeTheme) {
+    case "light":
+      applyThemeSettings(lightTheme);
+      break;
+    case "dark":
+      applyThemeSettings(darkTheme);
+      break;
+  }
+}
+
+function applyThemeSettings(theme) {
+  console.log(theme);
+  document.documentElement.style.setProperty("--theme-text-primary", theme.text.primary);
+  document.documentElement.style.setProperty("--theme-text-inverse", theme.text.inverse);
+  document.documentElement.style.setProperty("--theme-text-accent", theme.text.accent);
+
+  document.documentElement.style.setProperty("--theme-background-page", theme.background.page);
+  document.documentElement.style.setProperty("--theme-background-container", theme.background.container);
+  document.documentElement.style.setProperty("--theme-background-overlay", theme.background.overlay);
+
+  document.documentElement.style.setProperty("--theme-border-primary", theme.border.primary);
+  document.documentElement.style.setProperty("--theme-border-inverse", theme.border.inverse);
+}
+
+loadActiveTheme();
+
+const header = document.createElement("header");
+header.className = "header";
+
+const main = document.createElement("main");
+main.className = "main";
+
+const footer = document.createElement("footer");
+footer.className = "footer";
+
+document.body.append(header, main, footer);
---
+++ src/scripts/themes.js
@@ -0,0 +1,35 @@
+const lightTheme = {
+  text: {
+    primary: "#403f3d",
+    inverse: "#e1d4c9",
+    accent: "#b0907a"
+  },
+  background: {
+    page: "#e1d4c9",
+    container: "#665f55",
+    overlay: "#403F3DCC"
+  },
+  border: {
+    primary: "#c1b6ad",
+    inverse: "#665f55"
+  }
+};
+
+const darkTheme = {
+  text: {
+    primary: "#e1d4c9",
+    inverse: "#e1d4c9",
+    accent: "#b0907a"
+  },
+  background: {
+    page: "#292826",
+    container: "#403f3d",
+    overlay: "#292826CC"
+  },
+  border: {
+    primary: "#665f55",
+    inverse: "#c1b6ad"
+  }
+};
+
+export { darkTheme, lightTheme }
