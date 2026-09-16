723f51f389879def6f60d508fd77c1dfcf3c0d42
Andrei Nikiporchyk <andrewfroze@gmail.com>
2026-09-16 04:42:32 +0400

    feat: add contacts section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,6 +15,12 @@ import about4 from "../images/about-4.jpg";
 import mobileScreensImage from "../images/mobile-screens.png";
 import appStoreIcon from "../images/app-store.svg?raw";
 import googlePlayIcon from "../images/google-play.svg?raw";
+import pinIcon from "../images/pin-alt.svg?raw";
+import phoneIcon from "../images/phone.svg?raw";
+import clockIcon from "../images/clock.svg?raw";
+import twitterIcon from "../images/twitter.svg?raw";
+import instagramIcon from "../images/instagram.svg?raw";
+import facebookIcon from "../images/facebook.svg?raw";
 
 let currentSlide = 1;
 let activeTheme;
@@ -468,7 +474,7 @@ mobileOffer.className = "mobile-app__offer";
 
 const mobileTitle = document.createElement("h2");
 mobileTitle.className = "mobile-app__title";
-mobileTitle.textContent = "Download our app to start ordering";
+mobileTitle.innerHTML = "<em>Download</em> our app to start ordering";
 
 const description = document.createElement("p");
 description.className = "mobile-app__description";
@@ -530,3 +536,90 @@ mobileAppContainer.append(mobileOffer, mobileScreens);
 mobileApp.append(mobileAppContainer);
 
 main.append(mobileApp);
+
+const contacts = document.createElement("section");
+contacts.className = "contacts";
+contacts.id = "contacts";
+
+const contactsContainer = document.createElement("div");
+contactsContainer.className = "contacts__container";
+
+const contactsOffer = document.createElement("div");
+contactsOffer.className = "contacts__offer";
+
+const contactsTitle = document.createElement("h2");
+contactsTitle.className = "contacts__title";
+contactsTitle.innerHTML = "Sip, Savor, Smile. <em>It’s coffee time!</em>";
+
+const socials = document.createElement("div");
+socials.className = "contacts__socials";
+
+function createSocialButton(icon, label) {
+  const social = document.createElement("a");
+  social.className = "contacts__social";
+  social.href = "#";
+  social.setAttribute("aria-label", label);
+  social.innerHTML = icon;
+
+  return social;
+}
+
+socials.append(
+  createSocialButton(twitterIcon, "Twitter"),
+  createSocialButton(instagramIcon, "Instagram"),
+  createSocialButton(facebookIcon, "Facebook"),
+);
+
+contactsOffer.append(contactsTitle, socials);
+
+const contactsInfo = document.createElement("div");
+contactsInfo.className = "contacts__info";
+
+const contactsInfoTitle = document.createElement("h3");
+contactsInfoTitle.className = "contacts__info-title";
+contactsInfoTitle.textContent = "Contact us";
+
+const contactsList = document.createElement("div");
+contactsList.className = "contacts__list";
+
+function createContactLink(icon, text, href = "#") {
+  const link = document.createElement("a");
+  link.className = "contacts__link";
+  link.href = href;
+
+  const iconContainer = document.createElement("span");
+  iconContainer.className = "contacts__link-icon";
+  iconContainer.innerHTML = icon;
+
+  const textElement = document.createElement("span");
+  textElement.textContent = text;
+
+  link.append(iconContainer, textElement);
+
+  return link;
+}
+
+contactsList.append(
+  createContactLink(
+    pinIcon,
+    "8558 Green Rd., LA",
+    "#",
+  ),
+  createContactLink(
+    phoneIcon,
+    "+1 (603) 555-0123",
+    "tel:+16035550123",
+  ),
+  createContactLink(
+    clockIcon,
+    "Mon–Sat: 9:00–23:00",
+    "#",
+  ),
+);
+
+contactsInfo.append(contactsInfoTitle, contactsList);
+
+contactsContainer.append(contactsOffer, contactsInfo);
+contacts.append(contactsContainer);
+
+main.append(contacts);
